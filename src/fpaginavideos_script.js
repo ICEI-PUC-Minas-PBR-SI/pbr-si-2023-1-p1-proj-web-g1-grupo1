88bc55ab3e3413b1b31104ee23ceded2ceb1b3a4
Gabriel Rocha Lopes <gabriell-rocha@hotmail.com>
2023-06-21 12:59:22 -0300

    Fazendo alterações no style.css para melhor compreensão do código

--- src/fpaginavideos_script.js
+++ src/fpaginavideos_script.js
@@ -5,9 +5,9 @@ function exibirVideosWpp() {
             let str = ''
             for (let i = 0; i < data.length; i++) {
                 let videoswpp = data[i]
-                str += `<div class="video1">
-                <iframe width="340" height="190" src="${videoswpp.link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <p class="titulo1">${videoswpp.titulo}</p>
+                str += `<div class="videosPequenos">
+                <iframe width="560" height="315" src="${videoswpp.link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <p class="tituloVideosPequenos">${videoswpp.titulo}</p>
             </div>`
             }
             document.getElementById('exibirVideos').innerHTML = str
