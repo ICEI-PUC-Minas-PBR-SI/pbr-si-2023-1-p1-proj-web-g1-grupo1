f135df4ebd998ccc69c9f4a013f724b9096a66c9
Gabriel Rocha Lopes <gabriell-rocha@hotmail.com>
2023-06-22 10:15:44 -0300

    Adicionando aba de comentários

--- src/fpaginavideos_script.js
+++ src/fpaginavideos_script.js
@@ -8,6 +8,15 @@ function exibirVideosWpp() {
                 str += `<div class="videosPequenos">
                 <iframe width="560" height="315" src="${videoswpp.link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 <p class="tituloVideosPequenos">${videoswpp.titulo}</p>
+            </div>
+            <button class="button" onclick="toggleCommentBox()">Comentar</button>
+
+            <div id="commentBox" class="comment-box">
+              <span class="close-button" onclick="toggleCommentBox()">&#10005;</span>
+              <input type="text" id="commentInput" class="comment-input" placeholder="Digite seu comentário">
+              <button class="comment-submit" onclick="submitComment()">Enviar</button>
+          
+              <div id="commentSection"></div>
             </div>`
             }
             document.getElementById('exibirVideos').innerHTML = str
