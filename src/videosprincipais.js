a650ec4825f51ff345f727ac3e40697e7a53e0d1
Bruna Leticia <bruna_leticia07@hotmail.com>
2023-06-23 17:25:34 -0300

    finalizando

--- src/videosprincipais.js
+++ src/videosprincipais.js
@@ -10,15 +10,15 @@ function exibirVideosPrincipais() {
           const video = data[i];
   
           str += `
-            <div class="col-md-4">
-              <h2>${video.titulo}</h2>
-              <iframe width="340" height="190" src="${video.link}" frameborder="0" allowfullscreen></iframe>
-              <h4>${video.descricao}</h4>
-              <span class="fa fa-star checked"></span>
-              <span class="fa fa-star checked"></span>
-              <span class="fa fa-star checked"></span>
-            </div>
-          `;
+  <div class="col-md-4">
+   
+    <iframe width="340" height="190" src="${video.link}" frameborder="0" allowfullscreen></iframe>
+    <h5>${video.titulo}</h5>
+    <span class="fa fa-star checked"></span>
+    <span class="fa fa-star checked"></span>
+    <span class="fa fa-star checked"></span>
+  </div>
+`;
         }
   
         videosContainer.innerHTML = str;
