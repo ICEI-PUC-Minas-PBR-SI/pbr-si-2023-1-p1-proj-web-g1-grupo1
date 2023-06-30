92e554d9bf8c8f634934a71b429239c695bee857
Karine Gilberti <126530994+karinegilberti@users.noreply.github.com>
2023-06-29 21:21:12 -0300

    Subindo funcionalidade para os vídeos postados

---
+++ src/videospostados.js
@@ -0,0 +1,26 @@
+function getVideos() {
+    fetch('https://api-tiaw.vercel.app/geral')
+      .then(response => response.json())
+      .then(data => {
+        const videosContainer = document.getElementById('container-1');
+        
+        // Limpar o container de vídeos
+        videosContainer.innerHTML = '';
+
+        // Iterar sobre os vídeos e criar elementos HTML para exibi-los
+        data.forEach(video => {
+          const videoElement = document.createElement('iframe');
+          videoElement.src = video.url;
+          videoElement.width = '560';
+          videoElement.height = '315';
+
+          videosContainer.appendChild(videoElement);
+        });
+      })
+      .catch(error => {
+        console.log('Ocorreu um erro ao obter os vídeos:', error);
+      });
+  }
+
+  // Chamar a função para obter os vídeos quando a página for carregada
+  getVideos();
